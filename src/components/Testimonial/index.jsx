75d68b5aa9103c4a40a4e1ce3ec4866bbe9b1e1e
BasithPathiyil <basithpathiyil007@gmail.com>
2024-06-25 20:22:41 +0530

    changes

--- src/components/Testimonial/index.jsx
+++ src/components/Testimonial/index.jsx
@@ -14,8 +14,8 @@ export default function Testimonial({testimonialText, avatarName, avatarDesignat
         <Div className="cs-rating_bg" style={{backgroundImage: 'url(/images/rating.svg)'}} />
         <Div className="cs-rating_percentage" style={{backgroundImage: 'url(/images/rating.svg)', width:`${ratings * 20}%`}} />
       </Div>
-      <h2 className="cs-testimonial_avatar_name">{avatarName}</h2>
-      <Div className="cs-testimonial_avatar_designation text-uppercase">{avatarDesignation}</Div>
+      <h2 className="cs-testimonial_avatar_name  cs-white_color">{avatarName}</h2>
+      <Div className="cs-testimonial_avatar_designation text-uppercase cs-white_color">{avatarDesignation}</Div>
     </Div>
   )
 }
