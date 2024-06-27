651b03b2087dbb1eef0edb527670bb6e6e923302
BasithPathiyil <basithpathiyil007@gmail.com>
2024-06-28 03:09:47 +0530

    changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const Constants = {
-  imagebase: "http://3.93.169.114/uploads/",
+  // imagebase: "http://3.93.169.114/uploads/",
+  // imagebase: "http://localhost:5000/uploads/",
+  imagebase: "/ima/uploads/",
 };
